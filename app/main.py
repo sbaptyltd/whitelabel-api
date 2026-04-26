728c51425139d43459f7b9ee66aa326f2cbb6740
SBA <team@synergybits.com.au>
2026-04-27 01:29:35 +1000

    changes thene

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # TODO: restrict in production
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
